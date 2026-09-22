d90e5d32e10b675d5f6ea3d7b65c8858ee4a4073
Kaan Kırant <kaankirant98@gmail.com>
2026-09-22 06:06:28 +0300

    Add per-frame item trace to PlayerController
    Enable ticking on AInv_PlayerController and add per-frame TraceForItem to detect actors under the crosshair. Implements DeprojectScreenToWorld + LineTraceSingleByChannel using new configurable properties TraceLength (default 500) and ItemTraceChannel, and tracks ThisActor/LastActor (with start/stop log messages). Adds Kismet/GameplayStatics include and updates BP_Inv_PlayerController uasset accordingly.

--- Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
+++ Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
@@ -14,6 +14,9 @@ UCLASS()
 class INVENTORY_API AInv_PlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	AInv_PlayerController();
+	virtual void Tick(float DeltaSeconds) override;
 protected:
 	virtual void BeginPlay() override;	
 	virtual void SetupInputComponent() override;
@@ -21,6 +24,7 @@ protected:
 private:
 	void PrimaryInteract();
 	void CreateHUDWidget();
+	void TraceForItem();
 	
 	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
 	TArray<TObjectPtr<UInputMappingContext>> DefaultIMCs;
@@ -33,4 +37,13 @@ private:
 	
 	UPROPERTY()
 	TObjectPtr<UInv_HUDWidget> HUDWidget;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	double TraceLength;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TEnumAsByte<ECollisionChannel> ItemTraceChannel;
+	
+	TWeakObjectPtr<AActor> ThisActor;
+	TWeakObjectPtr<AActor> LastActor;
 };
