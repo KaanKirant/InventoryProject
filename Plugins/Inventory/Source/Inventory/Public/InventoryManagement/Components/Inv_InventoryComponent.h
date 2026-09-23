6f0f8cc18cfb98933914245e18b55410c1063784
Kaan Kırant <kaankirant98@gmail.com>
2026-09-23 03:10:50 +0300

    Add inventory toggle and menu open/close
    Add ToggleInventoryMenu, OpenInventoryMenu and CloseInventoryMenu to UInv_InventoryComponent (visibility, bInventoryMenuOpen, and input mode / cursor handling). ConstructInventory now creates the widget and closes it by default. AInv_PlayerController now finds the inventory component, exposes ToggleInventory (BlueprintCallable), and binds ToggleInventoryAction in SetupInputComponent. Also updated input mapping and player controller blueprint assets.

--- Plugins/Inventory/Source/Inventory/Public/InventoryManagement/Components/Inv_InventoryComponent.h
+++ Plugins/Inventory/Source/Inventory/Public/InventoryManagement/Components/Inv_InventoryComponent.h
@@ -16,6 +16,8 @@ class INVENTORY_API UInv_InventoryComponent : public UActorComponent
 
 public:
 	UInv_InventoryComponent();
+	
+	void ToggleInventoryMenu();
 
 protected:
 	virtual void BeginPlay() override;
@@ -31,4 +33,8 @@ private:
 	
 	UPROPERTY(EditAnywhere, Category = "Inventory")
 	TSubclassOf<UInv_InventoryBase> InventoryMenuClass;
+	
+	bool bInventoryMenuOpen;
+	void OpenInventoryMenu();
+	void CloseInventoryMenu();
 };
