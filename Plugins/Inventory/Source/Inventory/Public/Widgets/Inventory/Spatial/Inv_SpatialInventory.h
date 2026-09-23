f99deb471e524ab1d252843235bdea828b185801
Kaan Kırant <kaankirant98@gmail.com>
2026-09-23 12:17:01 +0300

    Add grid tab buttons and switching to spatial inventory
    Expose Equippables/Consumables/Craftables buttons in the spatial inventory widget and implement tab switching. Header: forward-declare UButton, add BindWidget pointers for three buttons, declare NativeOnInitialized and handlers (ShowEquippables/ShowConsumables/ShowCraftables), plus DisableButton and SetActiveGrid. CPP: bind button OnClicked events, implement showing the appropriate grid via a WidgetSwitcher and enable/disable buttons accordingly. Also updates the UAsset for the widget layout.

--- Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Spatial/Inv_SpatialInventory.h
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Spatial/Inv_SpatialInventory.h
@@ -6,6 +6,7 @@
 #include "Widgets/Inventory/InventoryBase/Inv_InventoryBase.h"
 #include "Inv_SpatialInventory.generated.h"
 
+class UButton;
 class UWidgetSwitcher;
 class UInv_InventoryGrid;
 
@@ -16,6 +17,8 @@ UCLASS()
 class INVENTORY_API UInv_SpatialInventory : public UInv_InventoryBase
 {
 	GENERATED_BODY()
+public:
+	virtual void NativeOnInitialized() override;
 	
 private:
 	
@@ -30,4 +33,25 @@ private:
 	
 	UPROPERTY(meta=(BindWidget))
 	TObjectPtr<UInv_InventoryGrid> Grid_Craftables;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> Button_Equippables;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> Button_Consumables;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> Button_Craftables;
+	
+	UFUNCTION()
+	void ShowEquippables();
+	
+	UFUNCTION()
+	void ShowConsumables();
+	
+	UFUNCTION()
+	void ShowCraftables();
+	
+	void DisableButton(UButton* Button);
+	void SetActiveGrid(UInv_InventoryGrid* Grid, UButton* Button);
 };
