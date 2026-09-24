56eea3eed50ff5dabe074abf33e91663fc1eca56
Kaan Kırant <kaankirant98@gmail.com>
2026-09-24 19:15:18 +0300

    Add replicated inventory item manifests
    This change introduces replicated inventory item manifests and fast-array inventory entries so items can be created from manifests and synchronized across the network. It adds lifetime replication for inventory components and item objects, registers replicated subobjects for inventory entries, and wires the server-side flow for adding new items to the inventory.

--- Plugins/Inventory/Source/Inventory/Public/InventoryManagement/Components/Inv_InventoryComponent.h
+++ Plugins/Inventory/Source/Inventory/Public/InventoryManagement/Components/Inv_InventoryComponent.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "InventoryManagement/FastArray/Inv_FastArray.h"
 #include "Inv_InventoryComponent.generated.h"
 
 
@@ -22,6 +23,7 @@ class INVENTORY_API UInv_InventoryComponent : public UActorComponent
 
 public:
 	UInv_InventoryComponent();
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
 
 	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Inventory")
 	void TryAddItem(UInv_ItemComponent* ItemComponent);
@@ -33,6 +35,7 @@ public:
 	void Server_AddStacksToItem(UInv_ItemComponent* ItemComponent, int32 StackCount, int32 Remainder);
 	
 	void ToggleInventoryMenu();
+	void AddRepSubObj(UObject* SubObj);
 	
 	FInventoryItemChange OnItemAdded;
 	FInventoryItemChange OnItemRemoved;
@@ -46,6 +49,9 @@ private:
 	TWeakObjectPtr<APlayerController> OwningController;
 	
 	void ConstructInventory();
+	
+	UPROPERTY(Replicated)
+	FInv_InventoryFastArray InventoryList;
 	
 	UPROPERTY()
 	TObjectPtr<UInv_InventoryBase> InventoryMenu;
