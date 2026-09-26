f851972c6ee0406f1793d38b569052414aed7b89
Kaan Kırant <kaankirant98@gmail.com>
2026-09-26 17:14:37 +0300

    Range-for with early exit in inventory grid
    Replace index-based iteration over GridSlots with a range-based for loop and add an early exit when AmountToFill reaches zero. Minor whitespace/formatting cleanup; no functional logic changes intended.

--- Plugins/Inventory/Source/Inventory/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
@@ -49,7 +49,12 @@ FInv_SlotAvailabilityResult UInv_InventoryGrid::HasRoomForItem(const FInv_ItemMa
 	
 	
 	// For each grid slot:
+	for (const auto& GridSlot: GridSlots)
+	{
 		// If we don't have anymore to fill, break out of the loop early.
+		if (AmountToFill == 0) break;
+		
+		
 		// Is this index claimed yet?
 		// Can the item fit here? (i.e. is it out of grid bounds?)
 		// Is there a room at this index? (i.e. are there other items in the way?)
@@ -61,6 +66,7 @@ FInv_SlotAvailabilityResult UInv_InventoryGrid::HasRoomForItem(const FInv_ItemMa
 			// If stackable, is this slot at the max stack size already?	
 		// How much to fill?
 		// Update the amount left to fill.
+	}
 	// How much is the remainder?
 	
 	return Result;
